c33eab6bd296810343d6d78e58e6dd28c7d294f9
Steve Marsh <Zimdale@gmail.com>
2014-01-08 19:13:26 -0500

    Created structure of class

--- schtasks.php
+++ schtasks.php
@@ -1,5 +1,32 @@
 <?php
     class schtasks {
         
+            public static function removeTask() {
+                
+            }
+            
+            public static function createTask() {
+                
+            }
+            
+            public static function modifyTask() {
+                
+            }
+            
+            public static function runTask() {
+                
+            }
+            
+            public static function getAllTasks() {
+                
+            }
+            
+            public static function getTaskInfo() {
+                
+            }
+            
+            public static function disableTask() {
+                
+            }
     }
 ?>
